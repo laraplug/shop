3864ae4658ce42bcf6c88d50f9488a681503dc07
Rajesh <mandaviya_rajesh@ymail.com>
2019-06-30 13:37:23 +0530

    Language translation
    Added language translation for the payments page

--- Resources/lang/en/payments.php
+++ Resources/lang/en/payments.php
@@ -1,8 +1,28 @@
 <?php
 
 return [
+	'gateways' => [
+        'direct_pay' => 'Direct pay',
+    ],
     'methods' => [
         'direct_bank' => 'Direct Bank',
         'card' => 'Credit/Check Card',
-    ]
+    ],
+    'messages' => [
+        'cannot pay' => 'I can not make a payment',
+        'checking for deposit' => 'I am confirming the deposit',
+        'payment pending order exists' => 'I have an unpaid order',
+        'waiting for approval' => 'Your payment is verifying your order',
+        'direct bank terms' => 'Payment will be made after confirmation of payment.',
+        'pay succeed' => 'Payment is complete',
+        'cannot cancel' => 'This order can not be canceled',
+    ],
+    'bank_info' => 'Deposit account information',
+    'bank_infos' => [
+        'bank_name' => 'Bank name',
+        'account_number' => 'Account Number',
+        'account_name' => 'Account Holder',
+    ],
+    'user cancel' => 'Cancel user',
 ];
+
