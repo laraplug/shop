8faf97219056ea919db9f48bf20bc7037ad53bea
wgh17 <wgh17@naver.com>
2022-11-14 08:24:46 +0900

    [잉리결제용페이지] 결제페이지 셋팅 request 설정 3

--- Http/Controllers/OrderController.php
+++ Http/Controllers/OrderController.php
@@ -217,7 +217,7 @@ class OrderController extends BasePublicController
      */
     public function storeEnReOrder(Request $request)
     {
-        $data = $request->data;
+        $data = $request;
 //        $data['shop_id'] = 1;
 //        $data['user_id'] = 1;
 //        $data['payment_name'] = 'EnReUtilityMall';
