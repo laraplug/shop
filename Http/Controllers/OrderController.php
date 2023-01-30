28c32de634ce06cd7ca08cea7227919ea8d31650
wgh17 <wgh17@naver.com>
2023-01-30 17:20:59 +0900

    file 이름 수정

--- Http/Controllers/OrderController.php
+++ Http/Controllers/OrderController.php
@@ -237,6 +237,8 @@ class OrderController extends BasePublicController
         $data['shipping_custom_field'] = $request->cartToken;
         $data['items'] = [];
         $order = $this->order->create($data);
+        $this->order->name = $request->itemName;
+
         $payButtonOnClick = $order->payment_gateway->getPayButtonOnClick();
         $gatewayView = $order->payment_gateway->preparePayment(route('shop.order.pay.store', $order->id));
         return view('shop.order.enrePay',compact('request','payButtonOnClick','gatewayView'));
