e5584f02f803254ea84115c829d22cdcd793e065
왕규호 <wgh8020@nate.com>
2024-08-05 22:38:45 +0900

    [OrderError 설정] EnreUtilityMall 계정 수정3

--- Http/Controllers/MyController.php
+++ Http/Controllers/MyController.php
@@ -108,18 +108,11 @@ class MyController extends BasePublicController
         $order->items->map(function($item) use ($items, $degreeCount) {
             $product_name = $item->product->name;
             echo '<script>';
-            echo 'console.log("start1")';
+            echo 'console.log("'.$items.'")';
             echo '</script>';
             if($items->count() > 0) {
                 $items->map(function($item2) use ($items, $item, $product_name) {
 
-                    echo '<script>';
-                    echo 'console.log("'.$item->product_id.'")';
-                    echo '</script>';
-
-                    echo '<script>';
-                    echo 'console.log("start2")';
-                    echo '</script>';
                 // 학사관리 항목이라면 카운트 추가
                 if($item->product_id == 1) {
                   $item2['글로벌학사관리교육활동과정']->quantity = $item2['글로벌학사관리교육활동과정']->quantity + 1;
